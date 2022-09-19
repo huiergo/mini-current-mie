c827e0e2339a7a2edebb791baee0e9f2402cd84e
huiergo <1063229610@qq.com>
2022-09-19 14:43:58 +0800

    feat: clearRect

--- js/main.js
+++ js/main.js
@@ -10,8 +10,6 @@ const ctx = canvas.getContext('2d')
 const databus = new DataBus()
 
 const BG_IMG_SRC = 'images/bg.jpg'
-const BG_WIDTH = 512
-const BG_HEIGHT = 512
 /**
  * 游戏主函数
  */
@@ -115,36 +113,7 @@ export default class Main {
    * 每一帧重新绘制所有的需要展示的元素
    */
   render() {
-    // ctx.clearRect(0, 0, canvas.width, canvas.height)
-
-    // // this.bg.render(ctx)
-
-    // databus.bullets
-    //   .concat(databus.enemys)
-    //   .forEach((item) => {
-    //     item.drawToCanvas(ctx)
-    //   })
-
-    // this.player.drawToCanvas(ctx)
-
-    // databus.animations.forEach((ani) => {
-    //   if (ani.isPlaying) {
-    //     ani.aniRender(ctx)
-    //   }
-    // })
-
-    // this.gameinfo.renderGameScore(ctx, databus.score)
-
-    // // 游戏结束停止帧循环
-    // if (databus.gameOver) {
-    //   this.gameinfo.renderGameOver(ctx, databus.score)
-
-    //   if (!this.hasEventBind) {
-    //     this.hasEventBind = true
-    //     this.touchHandler = this.touchEventHandler.bind(this)
-    //     canvas.addEventListener('touchstart', this.touchHandler)
-    //   }
-    // }
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
 
     this.box.renderBoxData()
   }
